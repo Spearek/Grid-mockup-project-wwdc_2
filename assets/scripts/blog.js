9e0d104ea6a56969442315a6798cce93023e8c7b
Spearek <30389214+Spearek@users.noreply.github.com>
2019-09-03 22:56:50 +0200

    blog section post images changed

---
+++ assets/scripts/blog.js
@@ -0,0 +1,4 @@
+const posts = document.querySelectorAll('.post');
+
+posts[0].style.backgroundImage = "url('./assets/img/blog_smoothie.jpg')";
+posts[1].style.backgroundImage = "url('./assets/img/blog_peas.jpg')";
