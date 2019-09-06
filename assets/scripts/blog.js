d1bc7fce5a52116aabe2c8c0a43dbe13be8965b0
Spearek <30389214+Spearek@users.noreply.github.com>
2019-09-06 22:21:59 +0200

    fixed blog.json path

--- assets/scripts/blog.js
+++ assets/scripts/blog.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const posts = document.querySelectorAll('.post');
-const url='/blog.json';
+const url='./blog.json';
 const blogBtn = document.querySelector('.button--blog');
 const postsContainer = document.querySelector('#posts-container');
 
@@ -9,7 +9,7 @@ const newPostHandler = () =>{
     let post =document.createElement('div');
     let domString= '<div class="post"><div class="post__date"><p>14<br/> NOV</p></div><div class="post__summary"><h3 class="post__summary__title">Growing Food and a "Thirst" for Innovation</h3><p class="post__summary__author">By Auskteez - 3 hours ago</p></div></div>';
     post.innerHTML = domString;
-    document.body.appendChild(post.firstChild); 
+    document.postsContainer.appendChild(post.firstChild); 
 };
 
 
